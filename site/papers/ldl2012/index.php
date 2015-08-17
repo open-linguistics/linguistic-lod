2156d276e05c83bf31eeffdb0fb221d06f3faa61
John P. McCrae <john@mccr.ae>
2015-08-17 14:01:12 +0100

    add index page

---
+++ site/papers/ldl2012/index.php
@@ -0,0 +1,90 @@
+<?php
+    $title = "1st Workshop on Linked Data in Linguistics";
+    $current = "other";
+    include 'header';
+?>
+   <section id="intro">
+
+      <div class="row">
+        
+        <div class="twelve columns">
+        <div class="slider-text">
+
+        <h1>1st Workshop on Linked Data in Linguistics</h1>
+
+        <h3 style="color:#fff;">Co-located with the 34<sup>th</sup> Annual Meeting of the German
+Society for Linguistics (DGfS), March 7<sup>th</sup>-9<sup>th</sup>, Frankfurt am Main, Germany</h3>
+
+        <h3 style="color:#fff;">Organized by: Sebastian Hellmann, Christian Chiarcos and Sebstian Nordhoff</h3>
+
+        </div></div></div>
+
+    </section>
+    <section>
+        <div class="row">
+
+        <div class="twelve columns">
+
+        <h2>Proceedings</h2>
+
+        <ul>
+            <li><b>Introduction and Overview</b>, <i>Christian Chiarcos and Sebastian Hellmann and Sebastian Nordhoff</i>
+[<a href="intro.pdf">Paper</a>]</li>
+
+        <li><b>Treating Dictionaries as a Linked-Data Corpus</b>, <i>Peter Bouda and Michael Cysouw</i>
+[<a href="bouda.pdf">Paper</a>]  [<a href="slides/bouda_slides.pdf">Slides</a>]</li>
+
+        <li><b>Integrating WordNet and Wiktionary with <i>lemon</i></b>, <i>John McCrae and Elena Montiel-Ponsoda and Philipp Cimiano</i>
+[<a href="mccrae.pdf">Paper</a>]  [<a href="slides/mccrae_slides.pdf">Slides</a>]</li>
+        
+        <li><b>Integrating lexical resources through an aligned lemma list</b>, <i>Axel Herold and Lothar Lemnitzer and Alexander Geyken</i>
+[<a href="herold.pdf">Paper</a>]  [<a href="slides/herold_slides.pdf">Slides</a>]</li>
+
+        <li><b>Linking Localisation and language Resources</b>, <i>David Lewis and Alexander O'Connor and Sebastian Molines and Leroy Finn and Dominic Jones and Stephen Curran and Séamus
+Lawless</i>
+[<a href="lewis.pdf">Paper</a>]</li>
+
+
+
+        <li><b>Reusing Linguistic Resources: Tasks and goals for a Linked Data approach</b>, <i>Marieke van Erp</i>
+[<a href="vanerp.pdf">Paper</a>]  [<a href="slides/van_erp_slides.pdf">Slides</a>]</li>
+        <li><b>A Discourse Information Radio News Database for Linguistic Analysis</b>, <i>Kerstin Eckart and Arndt Riester and Katrin Schweitzer</i>
+[<a href="eckart.pdf">Paper</a>]  [<a href="slides/eckart_slides.pdf">Slides</a>]</li>
+
+
+        <li><b>Integrating Treebank Annotation and User Activity Data in Translation research</b>, <i>Carl, Michael  and Høg Müller, Henrik</i>
+[<a href="carl.pdf">Paper</a>]  [<a href="slides/carl_slides.pdf">Slides</a>]</li>
+        <li><b>Creating Linked Data for the interdisciplinary international collaborative study of language acquisition and use:
+Achievements and challenges of a new Virtual Linguistics Lab
+</b>, <i>María Blume and Suzanne Flynn and Barbara Lust</i>
+[<a href="blume.pdf">Paper</a>]  [<a href="slides/blume_slides.pptx">Slides</a>]</li>
+        <li><b>Linking to linguistic data categories in ISOcat</b>, <i>Windhouwer, Menzo and Wright, Sue Ellen</i>
+[<a href="windhouwer.pdf">Paper</a>]  [<a href="slides/windhouwer_slides.pptx">Slides</a>]</li>
+
+        <li><b>Towards Linked Language Data  for Digital Humanities</b>, <i>Thierry Declerck and Piroska Lendvai and Karlheinz Mörth and Gerhard Budin and Tamás Váradi</i>
+[<a href="declerck.pdf">Paper</a>]  [<a href="slides/declerck_slides.ppt">Slides</a>]</li>
+
+        <li><b>OntoLingAnnot's ontologies: Facilitating interoperable linguistic annotations (up to the pragmatic level)</b>, <i>Antonio Pareja-Lora</i>
+[<a href="pareja.pdf">Paper</a>]  [<a href="slides/pareja_slides.pdf">Slides</a>]</li>
+        <li><b>Using Linked Data to create a typological knowledge base</b>, <i>Steven Moran</i>
+[<a href="moran.pdf">Paper</a>]  [<a href="slides/moran_slides.pdf">Slides</a>]</li>
+
+        <li><b>TYTO &mdash; A collaborative research tool for linked linguistic data</b>, <i>Schalley, Andrea C.</i>
+[<a href="schalley.pdf">Paper</a>]  [<a href="slides/schalley_slides.pdf">Slides</a>]</li>
+        <li><b>The Open Linguistics Working Group of the Open Knowledge Foundation</b>, <i>Christian Chiarcos and Sebastian Hellmann and Sebastian Nordhoff</i>
+[<a href="owlg.pdf">Paper</a>]</li>
+
+	<li><b>Interoperability of Corpora and Annotations</b>, <i>Christian Chiarcos</i>
+[<a href="chiarcos.pdf">Paper</a>]</li>
+
+        <li><b>The German DBpedia: A sense repository for linking entities</b>, <i>Sebastian Hellmann and Claus Stadler and Jens Lehmann</i>
+[<a href="hellmann.pdf">Paper</a>]</li>
+        <li><b>Linked Data for linguistic diversity research: Glottolog/Langdoc and ASJP</b>, <i>Sebastian Nordhoff</i>
+[<a href="nordhoff.pdf">Paper</a>]</li>
+        <li><b>Linking linguistic resources: Examples from the Open Linguistics Working Group</b>, <i>Christian Chiarcos and Sebastian Hellmann and Sebastian Nordhoff</i>
+[<a href="llod.pdf">Paper</a>]</li>
+
+      </div> <!-- Entries End -->
+
+   </section> <!-- Journal Section End-->
+<?php include 'footer'; ?>
