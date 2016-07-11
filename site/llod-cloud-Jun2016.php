dbbf6a6fde6b5d0379cb03bc9e2f815416c43c02
John P. McCrae <john@mccr.ae>
2016-07-11 17:20:11 +0100

    Add these images

---
+++ site/llod-cloud-Jun2016.php
@@ -0,0 +1,48 @@
+<?php 
+    $title = "LLOD Cloud (June 2016)";
+    include 'header';
+?>
+   <!-- Page Title
+   ================================================== -->
+   <div id="page-title">
+
+      <div class="row">
+
+         <div class="ten columns centered text-center">
+            <h1>The Linguistic Linked Open Data Cloud<span>.</span></h1>
+            <h3>June 2016</h3>
+
+            <p></p>
+         </div>
+
+      </div>
+
+   </div> <!-- Page Title End-->
+
+   <!-- Content
+   ================================================== -->
+   <div class="content-outer">
+
+      <div id="page-content" class="row page">
+
+         <div id="primary" class="twelve columns">
+
+            <section>
+
+                <img src="images/llod-cloud-Jun2016.svg"/>
+
+            </section> <!-- section end -->
+         <div class="four columns">
+            <h3><a href="llod-cloud-Jul2016.php">&lt;&lt; July 2016</a></h3>
+         </div>
+         <div class="four columns">
+            <h3 style="text-align:right;"><a href="llod-cloud-May2016.php">May 2016 &gt;&gt;</a></h3>
+         </div>
+
+
+         </div> <!-- primary end -->
+
+      </div> <!-- page-content End-->
+
+   </div> <!-- Content End-->
+<?php include 'footer'; ?>
