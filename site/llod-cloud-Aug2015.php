b580771911310d58ed715176fa6738d63c0eea9a
John P. McCrae <john@mccr.ae>
2017-06-26 05:02:26 -0400

    updates

---
+++ site/llod-cloud-Aug2015.php
@@ -0,0 +1,48 @@
+<?php 
+    $title = "LLOD Cloud (August 2015)";
+    include 'header';
+?>
+   <!-- Page Title
+   ================================================== -->
+   <div id="page-title">
+
+      <div class="row">
+
+         <div class="ten columns centered text-center">
+            <h1>The Linguistic Linked Open Data Cloud<span>.</span></h1>
+            <h3>August 2015</h3>
+
+            <p></p>
+         </div>
+
+      </div>
+
+   </div> <!-- Page Title End-->
+
+   <!-- Content
+   ================================================== -->
+   <div class="content-outer">
+
+      <div id="page-content" class="row page">
+
+         <div id="primary" class="twelve columns">
+
+            <section>
+
+                <img src="images/llod-cloud-Aug2015.svg"/>
+
+            </section> <!-- section end -->
+         <div class="four columns">
+            <h3><a href="llod-cloud-Sep2015.php">&lt;&lt; September 2015</a></h3>
+         </div>
+         <div class="four columns">
+            <h3 style="text-align:right;"><a href="llod-cloud-Aug2015.php">August 2015 &gt;&gt;</a></h3>
+         </div>
+
+
+         </div> <!-- primary end -->
+
+      </div> <!-- page-content End-->
+
+   </div> <!-- Content End-->
+<?php include 'footer'; ?>
---
+++ site/llod-cloud-May2017.php
@@ -0,0 +1,48 @@
+<?php 
+    $title = "LLOD Cloud (May 2017)";
+    include 'header';
+?>
+   <!-- Page Title
+   ================================================== -->
+   <div id="page-title">
+
+      <div class="row">
+
+         <div class="ten columns centered text-center">
+            <h1>The Linguistic Linked Open Data Cloud<span>.</span></h1>
+            <h3>May 2017</h3>
+
+            <p></p>
+         </div>
+
+      </div>
+
+   </div> <!-- Page Title End-->
+
+   <!-- Content
+   ================================================== -->
+   <div class="content-outer">
+
+      <div id="page-content" class="row page">
+
+         <div id="primary" class="twelve columns">
+
+            <section>
+
+                <img src="images/llod-cloud-May2017.svg"/>
+
+            </section> <!-- section end -->
+         <div class="four columns">
+            <h3><a href="llod-cloud-Jun2017.php">&lt;&lt; June 2017</a></h3>
+         </div>
+         <div class="four columns">
+            <h3 style="text-align:right;"><a href="llod-cloud-May2017.php">May 2017 &gt;&gt;</a></h3>
+         </div>
+
+
+         </div> <!-- primary end -->
+
+      </div> <!-- page-content End-->
+
+   </div> <!-- Content End-->
+<?php include 'footer'; ?>
